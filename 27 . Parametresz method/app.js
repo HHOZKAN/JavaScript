0048889bd1dcb45c83177ace4628ca89b925d962
HHOZKAN <ozkn.haci@gmail.com>
2023-11-03 17:22:28 +0100

    exo

---
+++ 27 . Parametresz method/app.js
@@ -0,0 +1,26 @@
+// Parametresiz ve geriye dondurmeyen metot tanimlamak
+
+/**
+ * 
+ * 
+ * 
+ */
+
+// function yazdir() {
+//     console.log("Enes")
+// }
+
+// yazdir();
+// debugger;
+// yazdir();
+// yazdir();
+// yazdir();
+// yazdir();
+// yazdir();
+// yazdir();
+
+function topla(){
+    console.log(5+7);
+}
+
+topla()
---
+++ 29 . Geriye deger donduren metotlar/app.js
@@ -0,0 +1,25 @@
+// // RETURN
+
+
+// /*
+
+//     1 - Bir degeri metodun disarisina cikarmak, tasimak icin kullanilir
+//     2 - Bir Metodu bitiren anahtar kelimedir.
+
+// */
+// let a = cube(5);
+// console.log(a)
+
+// let b = kare(a);
+// console.log(b)
+
+// function cube(sayi) {
+//     let sonuc = sayi**3;
+//     return sonuc;
+// }
+
+// function kare(sayi) {
+//     let sonuc = sayi**2
+//     return sonuc;
+// }
+
